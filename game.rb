a04b4c427b5c8baab461643a5e0e1fcbbd721dd3
carmtsang <carmen.tsang1208@hotmail.com>
2022-08-23 21:09:53 -0700

    add game, player, math. main requie files

---
+++ game.rb
@@ -0,0 +1,9 @@
+class Game
+  def initialize
+    @current_player = "Player 1"
+  end
+  def new_game
+    "This is a new game"
+  end
+end
+
---
+++ main.rb
@@ -0,0 +1,7 @@
+require './game.rb'
+require './player.rb'
+require './math.rb'
+
+game = Game.new
+
+puts game.new_game
