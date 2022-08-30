070b976a8e4c67da9e80afe18ef0cc566e7ebf7e
carmtsang <carmen.tsang1208@hotmail.com>
2022-08-29 23:20:32 -0700

    ask player for name when initialized

--- player.rb
+++ player.rb
@@ -1,9 +1,32 @@
 class Player
+  @@number_of_players = 0
+ 
   def initialize
-    number_of_lives = 3
+    @lives = 3
+    @@number_of_players += 1
+    ask_for_name
+    @name = gets.chomp
+  end
+
+  def ask_for_name
+    puts "Player #{@@number_of_players}, what is your name?"
+  end
+
+  def name
+    @name
+  end
+
+  def lives
+    @lives
+  end
+
+  def score
+    "#{name}: #{lives}/3"
   end
 
   def lose_game
-    number_of_lives -= 1
-  end  
+    @lives -=1
+  end
+
 end
+
